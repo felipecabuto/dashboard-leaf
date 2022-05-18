178e58abf4ce012d3b40b3b9bfaa50f9f473334a
Felipe Cabuto <felipecabuto@gmail.com>
2022-05-18 04:35:50 -0300

    Add files via upload

---
+++ js/orders.js
@@ -0,0 +1,33 @@
+const Orders = [
+    {
+        productName: 'Machine data',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Field boundaries',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Imagery',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Stara',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Raven',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+]
